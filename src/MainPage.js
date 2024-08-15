5687caafad8296b42937fde7362a2b336b58aa7c
ziji-z <zijizhouziji@gmail.com>
2024-08-14 23:23:14 -0700

    changed film/photo section. starts on writing

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,7 +3,7 @@ import './MainPage.css';
 import ContentLoader from './ContentLoader';
 
 const MainPage = () => {
-  const [title, setTitle] = useState('site');
+  const [title, setTitle] = useState('writings/for me, for school, and things to read');
   const [isArticleSelected, setIsArticleSelected] = useState(false);
 
   const changeTitle = (newTitle) => {
@@ -38,13 +38,14 @@ const MainPage = () => {
         <h1 className="title">ziji zhou | 周子霁</h1>
         <h2 className="subtitle">amherst college | github: ziji-z | zijizhouziji@gmail.com</h2>
         <div className='menu-note-container'>
-          <h3 id='menu-note'>none of the below are mutually exclusive</h3>
+          <h3 id='menu-note'>none of the below are mutually exclusive nor collectively exhaustive</h3>
         </div>
         <nav>
           <ul className="menu">
             <button className="menu-item" onClick={() => changeTitle('ziji/自己/things abt me')}>ziji/自己/things abt me</button>
             <button className="menu-item" onClick={() => changeTitle('art/paintings and sketches and things i made')}>art/paintings and sketches and things i made</button>
-            <button className="menu-item" onClick={() => changeTitle('film/photos and videos and things to see')}>film/photos and videos and things to see</button>
+            <button className="menu-item" onClick={() => changeTitle('photos/and things to see')}>photos/and things to see</button>
+            <button className="menu-item" onClick={() => changeTitle('film/and things to watch')}>film/and videos and things to see</button>
             <button className="menu-item" onClick={() => changeTitle('math/research and other things that involve logic of some sort')}>math/research and other things that involve logic of some sort</button>
             <button className="menu-item" onClick={() => changeTitle('sounds/and other stupid little things to listen to')}>sounds/and other stupid little things to listen to</button>
             <button className="menu-item" onClick={() => changeTitle('writings/for me, for school, and things to read')}>writings/for me, for school, and things to read</button>
